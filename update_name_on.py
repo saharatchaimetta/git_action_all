8a2e6e1124b2c3ab3f66411358d8995f4c7b74c3
MRPROPZ <propza56@gmail.com>
2026-02-24 10:41:07 +0700

    update_code_date

--- update_name_on.py
+++ update_name_on.py
@@ -23,6 +23,7 @@ def get_data_name_on():
     return data_code_name
 def get_shift_today():
     today = date.today()
+    # print(today)
     query_data_date = """
         SELECT 
             *
@@ -36,17 +37,23 @@ def get_shift_today():
             """
     params = {"today": today} 
     data_code_name = DatabaseConnection("BOT").get_data(query_data_date, **params)
+    # print_table(data_code_name)
     df_vertical = data_code_name.melt()
+    # print_table(df_vertical)
     df_vertical = df_vertical.rename(columns={"variable": "shift", "value": "code_name"})
     df_vertical = df_vertical[(df_vertical['shift'] != 'cafe_schedule') & (df_vertical['code_name'] != '-') & (df_vertical['shift'].str[0] != 'd')]
     df_vertical[['shift_type', 'shift_no']] = df_vertical['shift'].str.split('_', expand=True)
+    # print_table(df_vertical)
     mapping = {'day': 8, 'receive': 7}
     df_vertical['last_shift'] = (
         df_vertical['shift_no']
             .map(mapping)
             .fillna(pd.to_numeric(df_vertical['shift_no'], errors='coerce')))
     df_vertical = df_vertical.sort_values(by='last_shift', ascending=True).reset_index(drop=True)
+    # print_table(df_vertical)
     df_vertical = df_vertical[['code_name', 'shift', 'last_shift']]
+    df_vertical['last_update'] = today
+    # print_table(df_vertical)
     return df_vertical
 
     
@@ -56,8 +63,9 @@ def main():
     dataframe_name = get_data_name_on()
     # print_table(dataframe_name)
     dataframe_update = pd.merge(dataframe_name, dataframe_shift, on="code_name", how="left")
-    dataframe_update = dataframe_update[['id', 'code_name','shift', 'last_shift_y','last_update']]
-    dataframe_update = dataframe_update.rename(columns={"last_shift_y": "last_shift","shift": "remark"})
+    # print_table(dataframe_update)
+    dataframe_update = dataframe_update[['id', 'code_name','shift', 'last_shift_y','last_update_y']]
+    dataframe_update = dataframe_update.rename(columns={"last_shift_y": "last_shift","shift": "remark","last_update_y": "last_update"})
     dataframe_update['last_shift'] = dataframe_update['last_shift'].fillna(0).astype(int)
     # print_table(dataframe_update)
     DatabaseConnection("BOT").upsert_dataframe(dataframe_update,"user_name_bg_1000",["id"],"temp_user_name_bg_1000",1000)
